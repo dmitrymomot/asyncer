cd195a56727979dba4856bc33286351cd0b14c34
Dmitry Momot <mail@dmomot.com>
2024-01-18 14:45:55 +0200

    Add errors.go file with predefined errors

---
+++ errors.go
@@ -0,0 +1,14 @@
+package asyncer
+
+import "errors"
+
+// Predefined errors.
+var (
+	ErrFailedToParseRedisURI            = errors.New("failed to parse redis connection string")
+	ErrMissedAsynqClient                = errors.New("missed asynq client")
+	ErrFailedToCreateEnqueuerWithClient = errors.New("failed to create enqueuer with asynq client")
+	ErrFailedToEnqueueTask              = errors.New("failed to enqueue task")
+	ErrFailedToCloseEnqueuer            = errors.New("failed to close enqueuer")
+	ErrFailedToStartQueueServer         = errors.New("failed to start queue server")
+	ErrFailedToUnmarshalPayload         = errors.New("failed to unmarshal payload")
+)
