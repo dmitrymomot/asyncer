1d45aa770d0f7e7a6f929a94de80917cc17ca74c
Dmytro Momot <mail@dmomot.com>
2024-05-24 23:32:24 +0300

    chore: Add new task handler for greeting messages to check running of more then one task handler

--- example/queue/main.go
+++ example/queue/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/dmitrymomot/asyncer"
@@ -10,17 +14,28 @@ import (
 )
 
 const (
-	redisAddr    = "redis://localhost:6379/0"
-	TestTaskName = "queued_task"
+	redisAddr     = "redis://localhost:6379/0"
+	TestTaskName  = "queued_task"
+	TestTaskName2 = "queued_task_2"
 )
 
 type TestTaskPayload struct {
 	Name string
 }
 
+type TestTaskPayload2 struct {
+	Greeting string
+}
+
 // test task handler function
-func testTaskHandler(ctx context.Context, payload TestTaskPayload) error {
+func testTaskHandler(_ context.Context, payload TestTaskPayload) error {
 	fmt.Printf("Hello, %s!\n", payload.Name)
+	return nil
+}
+
+// test task handler function
+func testTaskHandler2(_ context.Context, payload TestTaskPayload2) error {
+	fmt.Printf("Hola, %s!\n", payload.Greeting)
 	return nil
 }
 
@@ -34,24 +49,61 @@ func main() {
 	eg.Go(asyncer.RunQueueServer(
 		ctx, redisAddr, nil,
 		// Register a handler for the task.
-		asyncer.HandlerFunc[TestTaskPayload](TestTaskName, testTaskHandler),
+		asyncer.HandlerFunc(TestTaskName, testTaskHandler),
+		asyncer.HandlerFunc(TestTaskName2, testTaskHandler2),
 		// ... add more handlers here ...
 	))
 
 	// Create a new enqueuer with redis as the broker.
-	enqueuer := asyncer.MustNewEnqueuer(redisAddr)
+	enqueuer := asyncer.MustNewEnqueuer(
+		redisAddr,
+		asyncer.WithTaskDeadline(10*time.Minute),
+		asyncer.WithMaxRetry(0),
+	)
 	defer enqueuer.Close()
 
 	// Enqueue a task with payload.
 	// The task will be processed after immediately.
-	for i := 0; i < 10; i++ {
-		if err := enqueuer.EnqueueTask(ctx, TestTaskName, TestTaskPayload{
-			Name: fmt.Sprintf("Test %d", i),
-		}); err != nil {
-			panic(err)
+	eg.Go(func() error {
+		var i int
+		ticker := time.NewTicker(1 * time.Second)
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case <-ticker.C:
+				i++
+				if err := enqueuer.EnqueueTask(ctx, TestTaskName, TestTaskPayload{
+					Name: fmt.Sprintf("Test %d", i),
+				}); err != nil {
+					return err
+				}
+				if err := enqueuer.EnqueueTask(ctx, TestTaskName2, TestTaskPayload2{
+					Greeting: fmt.Sprintf("Greeter %d", i),
+				}); err != nil {
+					return err
+				}
+			}
 		}
-		time.Sleep(500 * time.Millisecond)
-	}
+	})
+
+	// Listen for signals to cancel the context.
+	// This will stop the routine and close the queue server.
+	eg.Go(func() error {
+		c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+		select {
+		case <-c:
+			log.Println("Shutting down...")
+			// Cancel the context
+			cancel()
+
+			return nil
+		case <-ctx.Done():
+			return nil
+		}
+	})
 
 	// Wait for the queue server to exit.
 	if err := eg.Wait(); err != nil {
